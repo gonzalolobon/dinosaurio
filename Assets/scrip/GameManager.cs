1344cae1ec91b684074e4eaae500cb215c790974
giuseppecuttichavez <giuseppecuttichavez@gmail.com>
2025-11-18 14:54:09 +0100

    hacer el game manager

---
+++ Assets/scrip/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] PERSONAJECONTROL PERSONAJECONTROL;
+    [SerializeField] GameObject imagenPerder;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update
+    {
+        if (PERSONAJECONTROL.vida <= 0)
+        {
+            imagenPerder.SetActive(true);
+            StartCoroutine(CargarCreditos());
+        }
+    }
+}
